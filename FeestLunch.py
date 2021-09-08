c543966539e2d570fe7ad1a80129ceba74295e9d
Straatmongol <99067444@mydavinci.nl>
2021-09-08 13:35:42 +0200

    Eerste commit!

---
+++ FeestLunch.py
@@ -0,0 +1,11 @@
+Aantalcroissantjes = 17
+CroissantjesPrijs = 0.39
+
+Aantalstokbroden = 2
+StokbrodenPrijs = 2.78
+
+Aantalkortingsbonnen = 3
+kortingsbonnen = 1.50
+
+print(Aantalcroissantjes*CroissantjesPrijs + Aantalstokbroden*StokbrodenPrijs - Aantalkortingsbonnen*kortingsbonnen)
+
---
+++ de Speelhal-dag.py
@@ -0,0 +1,7 @@
+AantalPersonen = 3
+Toegangsticket = 7.45
+
+VRtijd = 45
+Kostenper5minuten = 0.37
+
+print((Toegangsticket + VRtijd*Kostenper5minuten)*AantalPersonen)
