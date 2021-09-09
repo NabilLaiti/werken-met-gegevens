6f94609d205dbb011f024b97935a9f6aad4eda05
Straatmongol <99067444@mydavinci.nl>
2021-09-09 09:36:29 +0200

    Eerste commit!

--- FeestLunch.py
+++ FeestLunch.py
@@ -7,5 +7,4 @@ StokbrodenPrijs = 2.78
 Aantalkortingsbonnen = 3
 kortingsbonnen = 1.50
 
-print(Aantalcroissantjes*CroissantjesPrijs + Aantalstokbroden*StokbrodenPrijs - Aantalkortingsbonnen*kortingsbonnen)
-
+print(f'‘De feestlunch kost je bij de bakker {round(Aantalcroissantjes*CroissantjesPrijs + Aantalstokbroden*StokbrodenPrijs - Aantalkortingsbonnen*kortingsbonnen, 2)} euro voor de {Aantalcroissantjes} croissantjes en de {Aantalstokbroden} stokbroden als de {Aantalkortingsbonnen} kortingsbonnen nog geldig zijn!’')
--- de Speelhal-dag.py
+++ de Speelhal-dag.py
@@ -1,7 +1,7 @@
-AantalPersonen = 3
+AantalPersonen = 4
 Toegangsticket = 7.45
 
 VRtijd = 45
 Kostenper5minuten = 0.37
 
-print((Toegangsticket + VRtijd*Kostenper5minuten)*AantalPersonen)
+print(f'‘Dit geweldige dagje-uit met {AantalPersonen} mensen in de Speelhal met {VRtijd} minuten VR kost je maar {round(Toegangsticket + VRtijd*(Kostenper5minuten*AantalPersonen), 2)} euro’')
